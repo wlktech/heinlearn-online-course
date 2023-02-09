415ef2d8e181f628a2c1ccba4a6ff42eaa2e1811
wailinnkyaw03 <wailinnkyaw03@gmail.com>
2023-02-09 22:57:25 +0630

    a

--- blog/about.php
+++ blog/about.php
@@ -2,18 +2,20 @@
 include "navbar.php";
 ?>
 <!-- Page header with logo and tagline-->
-<header class="py-5 bg-light border-bottom mb-4 shadow">
+<header class="py-3 bg-light border-bottom mb-4 shadow">
     <div class="container">
         <div class="text-center my-5">
             <h1 class="fw-bolder">About Me</h1>
-            <p class="lead mt-5" align="justify">I would like to share about my learning experience and knowledge about error solving problems when I started learning about programming language and web developments. Actually, I started to join Web Developer Career since Feb 2022. I learnt Python Programming Language with Django, Java Programming Language, JavaScript Programming Language with ReactJS and VueJS and Finally PHP Programming with Laravel.</p>
         </div>
     </div>
 </header>
-<div class="container">
+<div class="container my-5">
     <div class="row">
-        <div class="col">
-            <img src="" alt="">
+        <div class="col-md-6">
+            <img src="./assets/proxy.jpeg" class="w-100 px-3 rounded" alt="">
+        </div>
+        <div class="col-md-6">
+            <p class="lead mt-5 px-3" align="justify">Hello, My Name is Wai Linn Kyaw. I am the author of this WLK-Blog. I have graduated BEng Civil Engineering in Chindwin-PSB-Institute via University of Portsmouth, UK and BA(English) in Dagon University, Yangon. I would like to share about my learning experience and knowledge about error solving problems when I started learning about programming language and web developments. Actually, I started to join Web Developer Career since Feb 2022. I learnt Python Programming Language with Django, Java Programming Language, JavaScript Programming Language with ReactJS and VueJS and Finally PHP Programming with Laravel.</p>
         </div>
     </div>
 </div>
--- blog/contact.php
+++ blog/contact.php
@@ -4,10 +4,23 @@ include "navbar.php";
 <!-- Page header with logo and tagline-->
 <header class="py-5 bg-light border-bottom mb-4">
     <div class="container">
-        <div class="text-center my-5">
-            <h1 class="fw-bolder">About Me</h1>
-            <p class="lead mb-0">I would like to share about my learning experience and knowledge about error solving problems when I started learning about programming language and web developments. Actually, I started to join Web Developer Career since Feb 2022. I learnt Python Programming Language with Django, Java Programming Language, JavaScript Programming Language with ReactJS and VueJS and Finally PHP Programming with Laravel.</p>
+        <div class="text-center my-3">
+            <h1 class="fw-bolder">Contact Me</h1>
         </div>
+        <div class="row">
+            <div class="col-md-6 p-5">
+            <iframe class="rounded shadow" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3818.383078798053!2d96.16870831418024!3d16.856934122324954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c193631e318d6b%3A0x989fcb3a2b8b438c!2sWLK-TECH!5e0!3m2!1sen!2smm!4v1675958926778!5m2!1sen!2smm" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-md-6 p-5">
+                <form action="">
+                    <input type="text" class="form-control mb-3" name="name" placeholder="Enter Your Name">
+                    <input type="email" class="form-control mb-3" name="email" placeholder="Enter Your Email">
+                    <textarea name="comment" id="" cols="30" rows="10" class="form-control mb-3" placeholder="Leave a comment"></textarea>
+                    <button class="btn btn-outline-dark">Submit</button>
+                </form>
+            </div>
+        </div>
+        
     </div>
 </header>
 
