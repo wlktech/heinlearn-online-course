38236535b0ebb2b7a5d11eb00daf5eceec599ded
wailinnkyaw03 <102975967+wailinnkyaw03@users.noreply.github.com>
2023-02-18 18:27:02 +0630

    a

--- QueryBuilder.php
+++ QueryBuilder.php
@@ -12,7 +12,7 @@ class QueryBuilder{
         $bind_values = implode(", :", array_keys($datas));
         $sql = "INSERT INTO $table($column_names) VALUES(:$bind_values)";
 
-        $stmt = $conn->prepare($sql);
+        $stmt = $this->conn->prepare($sql);
         foreach($datas as $key => &$value){
             $stmt->bindParam(":".$key, $value);
         }
@@ -22,7 +22,7 @@ class QueryBuilder{
     //read 
     public function getAll($table, $cols){
         $sql = 'SELECT '.$cols.' FROM '.$table;
-        $stmt = $conn->prepare($sql);
+        $stmt = $this->conn->prepare($sql);
         $stmt->execute();
         $result = $stmt->fetchAll();
         return $result;
@@ -31,7 +31,7 @@ class QueryBuilder{
     //innerJoin
     public function getJoin($table1, $cols, $table2, $table1_id, $table2_id){
         $sql = "SELECT ".$cols." FROM ".$table1." INNER JOIN ".$table2." ON ".$table1.".".$table1_id."=".$table2.".".$table2_id;
-        $stmt = $conn->prepare($sql);
+        $stmt = $this->conn->prepare($sql);
         $stmt->execute();
         $result = $stmt->fetchAll();
         return $result;
@@ -40,17 +40,27 @@ class QueryBuilder{
     //update
     public function get($table, $cols, $id){
         $sql = 'SELECT '.$cols.' FROM '.$table.' WHERE id= :id';
-        $stmt = $conn->prepare($sql);
+        $stmt = $this->conn->prepare($sql);
         $stmt->bindParam(":id", $id);
         $stmt->execute();
         $result = $stmt->fetch();
         return $result;
     }
 
+    public function update($table, $datas, $edits, $id){
+        $sql = "UPDATE $table SET $edits WHERE id=:id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        foreach($datas as $key => &$value){
+            $stmt->bindParam(":".$key, $value);
+        }
+        $stmt->execute();
+    }
+
     //delete
     public function delete($table, $id){
-        $sql = 'DELETE FROM '.$table.' WHERE id=:id';
-        $stmt = $conn->prepare($sql);
+        $sql = "DELETE FROM $table WHERE id=:id";
+        $stmt = $this->conn->prepare($sql);
         $stmt->bindParam(":id", $id);
         $stmt->execute();
         return true;
